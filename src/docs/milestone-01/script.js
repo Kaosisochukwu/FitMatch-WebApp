f3f318106f9907c50f5627ffecb27eae7a9d4e13
mahran12 <89672689+mahran12@users.noreply.github.com>
2024-04-02 15:11:38 -0400

    JS Dynamic images added

---
+++ src/docs/milestone-01/script.js
@@ -0,0 +1,40 @@
+var image = document.createElement('img');
+image.src = "./images/homepage.png";
+image.alt = "Home Page";
+image.width = "800";
+image.height = "500";
+var container = document.getElementById('homeContainer');
+container.appendChild(image);
+
+
+var image = document.createElement('img');
+image.src = "./images/partnerSearch.png";
+image.alt = "Partner Page";
+image.width = "800";
+image.height = "500";
+var container = document.getElementById('partnerContainer');
+container.appendChild(image);
+
+
+
+var image = document.createElement('img');
+image.src = "./images/workouttracking.png";
+image.alt = "Workout Tracking Page";
+image.width = "800";
+image.height = "500";
+var container = document.getElementById('workoutContainer');
+container.appendChild(image);
+
+
+var image = document.createElement('img');
+image.src = "./images/community.png";
+image.alt = "Community Page";
+image.width = "800";
+image.height = "500";
+var container = document.getElementById('communityContainer');
+container.appendChild(image);
+
+
+
+
+
